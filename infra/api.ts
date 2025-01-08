ac10a7c27153653d5c1904d6bd903aa5cb4f4d3c
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-08 12:26:50 -0330

    fixed a routing mistake

--- infra/api.ts
+++ infra/api.ts
@@ -1,6 +1,6 @@
 export const api = new sst.aws.ApiGatewayV2( "MyApi" );
 
 api.route("GET /notes", {
-  handler: "packages/functions/src/create.handler",
+  handler: "packages/functions/src/api.handler",
   runtime: "nodejs20.x",
 });
