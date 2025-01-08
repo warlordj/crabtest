81fb1529679be17d55958cb159f4e7ab9527c9d4
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-08 11:57:48 -0330

    removed a folder

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -3,6 +3,6 @@ import { Handler } from "aws-lambda";
 export const handler: Handler = async (_event) => {
   return {
     statusCode: 200,
-    body: `hello1234.`,
+    body: `hello12345.`,
   };
 };
