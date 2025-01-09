d1af141cad464dc7373c5ad7b70428fccd6edaaf
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-09 14:48:38 -0330

    trying to add a policy xd

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,5 +1,19 @@
 export const bucket = new sst.aws.Bucket("MyBucket", {
     access: "public",
+
+    transform: {
+        policy: (args) => {
+            args.policy = sst.aws.iamEdit(args.policy, (policy) => {
+                policy.Statement.push({
+                    Effect: "Allow",
+                    Principal: {Service: "ses.amazonaws.com"},
+                    Action: "s3:GetObject",
+                    Resource: $interpolate`arn:aws:s3:::${args.bucket}/*`,
+                })
+            })
+        }
+    }
+
 });
 
 export const exampleBucketPublicAccessBlock = new aws.s3.BucketPublicAccessBlock("MyBucketAccess", {
