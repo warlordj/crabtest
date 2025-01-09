69e1c38597c7f70253066fcc81029c9d0ce42339
Jeremy Lee <jeremy@keenpayroll.ca>
2025-01-09 14:39:25 -0330

    trying to make a public s3 - adding policy

--- infra/storage.ts
+++ infra/storage.ts
@@ -17,3 +17,25 @@ export const siteBucketWebsiteConfig = new aws.s3.BucketWebsiteConfigurationV2("
     },
     errorDocument: {key: "index.html",},
 });
+
+function publicReadPolicyForBucket(bucketName) {
+    return {
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Principal: "*",
+            Action: [
+                "s3:GetObject"
+            ],
+            Resource: [
+                `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
+            ]
+        }]
+    };
+}
+
+// Set the access policy for the bucket so all objects are readable
+let bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+    bucket: bucket.name, // refer to the bucket created earlier
+    policy: bucket.bucket.apply(publicReadPolicyForBucket) // use output property `siteBucket.bucket`
+}, { dependsOn: exampleBucketPublicAccessBlock });
